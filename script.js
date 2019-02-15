83c66ea2953e20994afa976bd31c6d22c6bf7ebe
freekjoosten <45119063+freekjoosten@users.noreply.github.com>
2019-02-15 21:22:28 +0100

    dus!
    Nu met toevoegingen!

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 let entireCalc = document.querySelector('.container');
 let screen = document.querySelector('.scherm');
 
+
+// Geklikt nummer naar het scherm
+
 entireCalc.addEventListener('click', (event) => {
     const button = event.target;
     console.log(button.classList);
 
-    if(button.classList.contains('number')){
-        screen.textContent = button.textContent;
+    if(button.classList.contains('number')){            
+            screen.textContent += button.textContent;
     }
 });
-
-// numberButtons.addEventListener('click', (event) => {
-//     const button = event.target;
-//     console.log(button.classList);
-  
-//     if(button.classList.contains('number')){
-//         screen.textContent = button.textContent;
-//     }
-// });
-
